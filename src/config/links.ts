554dc57b8a6f100bdd4909b0d6ef105adf3ee139
Dante20012002 <santiagod.puentesg@gmail.com>
2025-05-08 15:53:15 -0500

    Estructuración y responsive de pagina principal

---
+++ src/config/links.ts
@@ -0,0 +1,23 @@
+interface Link {
+  text: string;
+  href: string;
+}
+
+export const LINKS: Link[] = [
+  {
+    text: 'Inicio',
+    href: '/'
+  },
+  {
+    text: 'Servicios',
+    href: '/servicios'
+  },
+  {
+    text: 'Nosotros',
+    href: '/nosotros'
+  },
+  {
+    text: 'Contacto',
+    href: '/contacto'
+  }
+]; 
