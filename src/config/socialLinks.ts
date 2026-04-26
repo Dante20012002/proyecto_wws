7eca307154e886a80b657ac2739d3526f1723584
Dante20012002 <santiagod.puentesg@gmail.com>
2026-04-26 00:49:09 -0500

    Actualizar enlaces de redes sociales en el componente de contacto

--- src/config/socialLinks.ts
+++ src/config/socialLinks.ts
@@ -1,7 +1,7 @@
 const socialLinks = [
-  { icon: '/assets/sprite.svg#facebook', href: '#', alt: 'Facebook' },
-  { icon: '/assets/sprite.svg#whatsapp', href: '#', alt: 'Whatsapp' },
-  { icon: '/assets/sprite.svg#instagram', href: '#', alt: 'Instagram' },
+  // { icon: '/assets/sprite.svg#facebook', href: '#', alt: 'Facebook' },
+  { icon: '/assets/sprite.svg#whatsapp', href: 'https://wa.me/573107665112?text=Hola%2C%20quiero%20realizar%20una%20cotizaci%C3%B3n', alt: 'Whatsapp' },
+  // { icon: '/assets/sprite.svg#instagram', href: '#', alt: 'Instagram' },
 ];
 
 export default socialLinks;
